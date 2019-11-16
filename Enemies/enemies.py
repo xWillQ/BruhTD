31fea73b6a7bf33af72b80ffe63a5d3ba548c729
xWillQ <pasha.shevchenko.90@gmail.com>
2019-11-16 16:41:11 +0700

    Round turns

--- Enemies/enemies.py
+++ Enemies/enemies.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Enemy():
     def __init__(self, startX, startY):
         self.posX = startX
@@ -9,17 +12,91 @@ class Enemy():
         self.posX += self.velX
         self.posY += self.velY
 
-    def turn(self, direction):
-        velocity = abs(self.velX + self.velY)
-        if (direction == 0):
-            self.velX = velocity
-            self.velY = 0
-        elif (direction == 90):
-            self.velX = 0
-            self.velY = -velocity
-        elif (direction == 180):
-            self.velX = -velocity
-            self.velY = 0
-        elif (direction == 270):
-            self.velX = 0
-            self.velY = velocity
+    def turn(self, turnX, turnY, clockwise):
+        velocity = sqrt(self.velX**2 + self.velY**2)
+        d = sqrt((self.posX - turnX)**2 + (self.posY - turnY)**2)
+        a = (2 * (d**2) - velocity**2) / (2 * d)
+        h = sqrt(d**2 - a**2)
+        x2 = turnX + a * (self.posX - turnX) / d
+        y2 = turnY + a * (self.posY - turnY) / d
+        newX1 = x2 + h * (self.posY - turnY) / d
+        newY1 = y2 - h * (self.posX - turnX) / d
+        newX2 = x2 - h * (self.posY - turnY) / d
+        newY2 = y2 + h * (self.posX - turnX) / d
+        if clockwise:
+            if (self.posX >= turnX) and (self.posY > turnY):
+                currentSection = 1
+            elif (self.posX < turnX) and (self.posY >= turnY):
+                currentSection = 2
+            elif (self.posX <= turnX) and (self.posY < turnY):
+                currentSection = 3
+            elif (self.posX > turnX) and (self.posY <= turnY):
+                currentSection = 4
+            if currentSection == 1:
+                if newX1 > newX2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+            elif currentSection == 2:
+                if newY1 > newY2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+            elif currentSection == 3:
+                if newX1 < newX2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+            elif currentSection == 4:
+                if newY1 < newY2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+        else:
+            if (self.posX > turnX) and (self.posY >= turnY):
+                currentSection = 1
+            elif (self.posX <= turnX) and (self.posY > turnY):
+                currentSection = 2
+            elif (self.posX < turnX) and (self.posY <= turnY):
+                currentSection = 3
+            elif (self.posX >= turnX) and (self.posY < turnY):
+                currentSection = 4
+            if currentSection == 1:
+                if newY1 > newY2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+            elif currentSection == 2:
+                if newX1 < newX2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+            elif currentSection == 3:
+                if newY1 < newY2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+            elif currentSection == 4:
+                if newX1 > newX2:
+                    self.posX = newX1
+                    self.posY = newY1
+                else:
+                    self.posX = newX2
+                    self.posY = newY2
+        self.posX = int(self.posX)
+        self.posY = int(self.posY)
+        
--- Map/map.py
+++ Map/map.py
@@ -1,8 +1,7 @@
 class Turn():
     """Структура для хранения данных о повороте"""
-    def __init__(self, x, y, lengthX, lengthY, direction):
+    def __init__(self, x, y, radius, clockwise):
         self.x = x
         self.y = y
-        self.lengthX = lengthX
-        self.lengthY = lengthY
-        self.direction = direction
+        self.radius = radius
+        self.clockwise = clockwise
