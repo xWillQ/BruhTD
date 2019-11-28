fa6248ece1094e50c8d11a6a7d483b8d3d80c23e
xWillQ <pasha.shevchenko.90@gmail.com>
2019-11-28 22:58:25 +0700

    Changed enemies to work with new turns

--- Enemies/enemies.py
+++ Enemies/enemies.py
@@ -15,38 +15,38 @@ class Enemy():
 
     def turn(self, turn):
         velocity = sqrt(self.velX**2 + self.velY**2)
-        d = sqrt((self.posX - turn.x)**2 + (self.posY - turn.y)**2)
+        d = sqrt((self.posX - turn.circleX)**2 + (self.posY - turn.circleY)**2)
         a = (2 * (d**2) - velocity**2) / (2 * d)
         h = sqrt(d**2 - a**2)
-        x2 = turn.x + a * (self.posX - turn.x) / d
-        y2 = turn.y + a * (self.posY - turn.y) / d
-        newX1 = x2 + h * (self.posY - turn.y) / d
-        newY1 = y2 - h * (self.posX - turn.x) / d
-        newX2 = x2 - h * (self.posY - turn.y) / d
-        newY2 = y2 + h * (self.posX - turn.x) / d
+        x2 = turn.circleX + a * (self.posX - turn.circleX) / d
+        y2 = turn.circleY + a * (self.posY - turn.circleY) / d
+        newX1 = x2 + h * (self.posY - turn.circleY) / d
+        newY1 = y2 - h * (self.posX - turn.circleX) / d
+        newX2 = x2 - h * (self.posY - turn.circleY) / d
+        newY2 = y2 + h * (self.posX - turn.circleX) / d
         if turn.clockwise:
-            if (self.posX >= turn.x) and (self.posY < turn.y):
+            if (self.posX >= turn.circleX) and (self.posY < turn.circleY):
                 if newX1 > newX2:
                     self.posX = newX1
                     self.posY = newY1
                 else:
                     self.posX = newX2
                     self.posY = newY2
-            elif (self.posX < turn.x) and (self.posY <= turn.y):
+            elif (self.posX < turn.circleX) and (self.posY <= turn.circleY):
                 if newY1 < newY2:
                     self.posX = newX1
                     self.posY = newY1
                 else:
                     self.posX = newX2
                     self.posY = newY2
-            elif (self.posX <= turn.x) and (self.posY > turn.y):
+            elif (self.posX <= turn.circleX) and (self.posY > turn.circleY):
                 if newX1 < newX2:
                     self.posX = newX1
                     self.posY = newY1
                 else:
                     self.posX = newX2
                     self.posY = newY2
-            elif (self.posX > turn.x) and (self.posY >= turn.y):
+            elif (self.posX > turn.circleX) and (self.posY >= turn.circleY):
                 if newY1 > newY2:
                     self.posX = newX1
                     self.posY = newY1
@@ -54,28 +54,28 @@ class Enemy():
                     self.posX = newX2
                     self.posY = newY2
         else:
-            if (self.posX > turn.x) and (self.posY <= turn.y):
+            if (self.posX > turn.circleX) and (self.posY <= turn.circleY):
                 if newY1 < newY2:
                     self.posX = newX1
                     self.posY = newY1
                 else:
                     self.posX = newX2
                     self.posY = newY2
-            elif (self.posX <= turn.x) and (self.posY < turn.y):
+            elif (self.posX <= turn.circleX) and (self.posY < turn.circleY):
                 if newX1 < newX2:
                     self.posX = newX1
                     self.posY = newY1
                 else:
                     self.posX = newX2
                     self.posY = newY2
-            elif (self.posX < turn.x) and (self.posY >= turn.y):
+            elif (self.posX < turn.circleX) and (self.posY >= turn.circleY):
                 if newY1 > newY2:
                     self.posX = newX1
                     self.posY = newY1
                 else:
                     self.posX = newX2
                     self.posY = newY2
-            elif (self.posX >= turn.x) and (self.posY > turn.y):
+            elif (self.posX >= turn.circleX) and (self.posY > turn.circleY):
                 if newX1 > newX2:
                     self.posX = newX1
                     self.posY = newY1
@@ -85,28 +85,28 @@ class Enemy():
         self.posX = round(self.posX)
         self.posY = round(self.posY)
         if turn.clockwise:
-            if turn.endSection == 1:
+            if turn.section == 1:
                 self.velX = 0
                 self.velY = velocity
-            elif turn.endSection == 2:
+            elif turn.section == 2:
                 self.velX = velocity
                 self.velY = 0
-            elif turn.endSection == 3:
+            elif turn.section == 3:
                 self.velX = 0
                 self.velY = -velocity
-            elif turn.endSection == 4:
+            elif turn.section == 4:
                 self.velX = -velocity
                 self.velY = 0
             else:
-                if turn.endSection == 1:
+                if turn.section == 1:
                     self.velX = -velocity
                     self.velY = 0
-                elif turn.endSection == 2:
+                elif turn.section == 2:
                     self.velX = 0
                     self.velY = velocity
-                elif turn.endSection == 3:
+                elif turn.section == 3:
                     self.velX = velocity
                     self.velY = 0
-                elif turn.endSection == 4:
+                elif turn.section == 4:
                     self.velX = 0
                     self.velY = -velocity
