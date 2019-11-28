912aa759d6e702b3c5eeb27f2fa727d380bc58b7
xWillQ <pasha.shevchenko.90@gmail.com>
2019-11-28 22:37:38 +0700

    Improved turns

--- Map/map.py
+++ Map/map.py
@@ -2,33 +2,35 @@ from math import sqrt
 
 
 class Turn():
-    def __init__(self, x, y, radius, clockwise, startSection, endSection):
+    def __init__(self, x, y, transformation, clockwise, section):
         self.x = x
         self.y = y
-        self.radius = radius
+        self.radius = round(transformation * 75 / 100)
         self.clockwise = clockwise
-        self.startSection = startSection
-        self.endSection = endSection
+        self.section = section
+        if section == 1:
+            self.circleX = x
+            self.circleY = y + transformation
+        elif section == 2:
+            self.circleX = x + transformation
+            self.circleY = y + transformation
+        elif section == 3:
+            self.circleX = x + transformation
+            self.circleY = y
+        else:
+            self.circleX = x
+            self.circleY = y
 
     def isInside(self, x, y):
-        if (sqrt((x - self.x)**2 + (y - self.y)**2) > self.radius):
+        if (sqrt((x - self.circleX)**2 + (y - self.circleY)**2) > self.radius):
             return False
-        if (self.startSection == 1 and (x >= self.x and y <= self.y)):
+        if (self.section == 1 and (x >= self.circleX and y <= self.circleY)):
             return True
-        if (self.startSection == 2 and (x <= self.x and y <= self.y)):
+        if (self.section == 2 and (x <= self.circleX and y <= self.circleY)):
             return True
-        if (self.startSection == 3 and (x <= self.x and y >= self.y)):
+        if (self.section == 3 and (x <= self.circleX and y >= self.circleY)):
             return True
-        if (self.startSection == 4 and (x >= self.x and y >= self.y)):
-            return True
-        
-        if (self.endSection == 1 and (x >= self.x and y <= self.y)):
-            return True
-        if (self.endSection == 2 and (x <= self.x and y <= self.y)):
-            return True
-        if (self.endSection == 3 and (x <= self.x and y >= self.y)):
-            return True
-        if (self.endSection == 4 and (x >= self.x and y >= self.y)):
+        if (self.section == 4 and (x >= self.circleX and y >= self.circleY)):
             return True
 
         return False
